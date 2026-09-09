9334a4a8c81a861b2932461ad5ee46efa5bfd2b4
nicholas andrew eymann <thedao@nicholass-Mac-Studio.local>
2026-09-09 03:08:34 -0700

    feat(voc): Arcane Signet, Spectral Sailor

---
+++ mtg-sets/2017-2022/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/voc/cards/ArcaneSignetReprint.kt
@@ -0,0 +1,20 @@
+package com.wingedsheep.mtg.sets.definitions.voc.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/**
+ * Arcane Signet reprint in Innistrad: Crimson Vow Commander. Canonical
+ * [com.wingedsheep.sdk.model.CardDefinition] lives in BLC's `cards/` package.
+ */
+val ArcaneSignetReprint = Printing(
+    oracleId = "0bc7f093-bef0-4f1a-852c-4b75ebf54838",
+    name = "Arcane Signet",
+    setCode = "VOC",
+    collectorNumber = "159",
+    scryfallId = "46f17b66-2484-49f4-80db-408bfda95aa9",
+    artist = "Dan Murayama Scott",
+    imageUri = "https://cards.scryfall.io/normal/front/4/6/46f17b66-2484-49f4-80db-408bfda95aa9.jpg?1783924942",
+    releaseDate = "2021-11-19",
+    rarity = Rarity.COMMON,
+)
---
+++ mtg-sets/2017-2022/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/voc/cards/SpectralSailorReprint.kt
@@ -0,0 +1,20 @@
+package com.wingedsheep.mtg.sets.definitions.voc.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/**
+ * Spectral Sailor reprint in Innistrad: Crimson Vow Commander. Canonical
+ * [com.wingedsheep.sdk.model.CardDefinition] lives in M20's `cards/` package.
+ */
+val SpectralSailorReprint = Printing(
+    oracleId = "a8fdbcdf-479d-4582-9ad5-9fbd4c740c29",
+    name = "Spectral Sailor",
+    setCode = "VOC",
+    collectorNumber = "114",
+    scryfallId = "d6e1dd43-8ce8-4225-8a04-d6f071f5fb31",
+    artist = "Cristi Balanescu",
+    imageUri = "https://cards.scryfall.io/normal/front/d/6/d6e1dd43-8ce8-4225-8a04-d6f071f5fb31.jpg?1783924961",
+    releaseDate = "2021-11-19",
+    rarity = Rarity.UNCOMMON,
+)
